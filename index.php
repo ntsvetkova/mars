672b69ba7fbd0e1308e1a8b9ec17570705db2316
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-08-21 16:43:23 +0300

    Change: user input

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
 namespace Mars;
 
-$plateauRightCorner = $_POST["PlateauRightCorner"];
-$position = $_POST["Position"];
-$instructions = $_POST["Instructions"];
+require_once __DIR__ . '/controllers/SetData.php';
+$data = new SetData(explode("\n",$_POST["input"]));
+$data->execute();
 
-if (preg_match("/\d\s\d$/",$plateauRightCorner) && (preg_match("/(\d\s){2}[NSWE]{1}?$/",$position))
-    && (preg_match("/[LRM]$/",$instructions))) {
-    $position = str_split(str_replace(' ','',$position));
-    require_once __DIR__ . '/controllers/Actions.php';
-    $a = new Actions($instructions);
-    $a->change($position[0],$position[1],$position[2]);
-}
-
-//require_once __DIR__ . '/controllers/Actions.php';
-//$a = new Actions('LMLMLMLMM');
+//require_once __DIR__ . '/controllers/Action.php';
+//$a = new Action('LMLMLMLMM');
 //$a->change(1,2,'N');
-//$b = new Actions('MMRMMRMRRM');
+//$b = new Action('MMRMMRMRRM');
 //$b->change(3,3,'E');
